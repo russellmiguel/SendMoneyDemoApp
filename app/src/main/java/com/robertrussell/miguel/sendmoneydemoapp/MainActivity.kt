5f40f86c05efa275841706b686ed32c4f02d2177
russellmiguel <miguel.robertrussell@gmail.com>
2026-08-10 21:17:45 +0800

    Initial commit

---
+++ app/src/main/java/com/robertrussell/miguel/sendmoneydemoapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.robertrussell.miguel.sendmoneydemoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.robertrussell.miguel.sendmoneydemoapp.ui.theme.SendMoneyDemoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SendMoneyDemoAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SendMoneyDemoAppTheme {
+        Greeting("Android")
+    }
+}
